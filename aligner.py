d0998f59a3a4c56a86fd232d50d36143fc59c8da
zhourunlong <zhourunlongvector@gmail.com>
2021-06-10 18:04:53 +0800

    multiple file aligner

--- aligner.py
+++ aligner.py
@@ -1,6 +1,7 @@
 import srt
 import argparse
 import torch
+import os
 from semantic_matching.model import SemanticMatchingModel
 
 def get_args():
@@ -10,18 +11,50 @@ def get_args():
     return parser.parse_args()
 
 def caption_to_text(caption):
-    return [sub.content.replace("\n", "").replace("\r", "") for sub in caption]
+    return [sub.content.replace("\n", " ").replace("\r", " ") for sub in caption]
 
 if __name__ == "__main__":
     args = get_args()
 
-    with open("asset/RAW VIDEO_ Trump speaks at NC GOP convention - Part 1 - English (auto.srt") as f:
-        video_caption =  list(srt.parse(f.read()))
+    all_files = os.listdir("asset")
+    all_fn = []
+    for fn_w_ext in all_files:
+        fn = os.path.splitext(fn_w_ext)[0]
+        f1 = fn + ".mp4"
+        f2 = fn + ".srt"
+        if (not fn in all_fn) and (f1 in all_files) and (f2 in all_files):
+            all_fn.append(fn)
     
-    with open("asset/Rick-Astley-Never-Gonna-Give-You-Up.srt") as f:
+    if not "song" in all_fn:
+        print("Please put song.mp4 and song.srt in asset/ !")
+        exit(0)
+    
+    if len(all_fn) == 1:
+        print("Please put <video_name>.mp4 and <video_name>.srt in asset/ !")
+        exit(0)
+
+    print("Song: song.{mp4, srt}")
+    print("Video(s):")
+    for fn in all_fn:
+        if fn != "song":
+            print("\t" + fn + ".{mp4, srt}")
+
+    video_captions, video_fn = [], []
+    for fn in all_fn:
+        if fn != "song":
+            video_fn.append(fn)
+            with open("asset/" + fn + ".srt") as f:
+                video_captions.append(list(srt.parse(f.read())))
+    
+    with open("asset/song.srt") as f:
         audio_caption =  list(srt.parse(f.read()))
-    
-    video_text = caption_to_text(video_caption)
+
+    video_text, source = [], []
+    cnt = 0
+    for vcap in video_captions:
+        video_text += caption_to_text(vcap)
+        source += [cnt for _ in range(len(vcap))]
+        cnt += 1
     audio_text = caption_to_text(audio_caption)
 
     model = SemanticMatchingModel()
@@ -36,4 +69,5 @@ if __name__ == "__main__":
     for i in range(len(audio_text)):
         print(audio_text[i])
         for k in range(args.k):
-            print("\t", video_text[idx_topk[i, k]], sim_topk[i, k].item())
+            idx = idx_topk[i, k]
+            print("\t", video_text[idx], "%1.3f" % (sim_topk[i, k].item()), video_fn[source[idx]])
